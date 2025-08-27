86a6cb5d81a1ddded72efabaaac4a7a5a998e215
Rafael Filimberti <rfilimberti@gmail.com>
2025-08-27 17:23:36 -0300

    #41 toJson / fromJson

--- dart/api/example/api_example.dart
+++ dart/api/example/api_example.dart
@@ -2,16 +2,43 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-Future  main() async  {
-final json = await fetch();
-print(json['id']);
+Future main() async {
+  final todo = await fetch();
+  //print(todo.completed);
+  print(todo.toJson());
 }
 
-Future<Map> fetch() async {
+Future<Todo> fetch() async {
   var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
   var response = await http.get(url);
   var json = jsonDecode(response.body);
-  return json;
+  var todo = Todo.fromJson(json);
+  return todo;
 }
 
+class Todo {
+  final String title;
+  final int id;
+  final int userId;
+  final bool completed;
 
+  Todo({
+    required this.title,
+    required this.id,
+    required this.userId,
+    required this.completed,
+  });
+
+  factory Todo.fromJson(Map json) {
+    return Todo(
+      title: json['title'],
+      id: json['id'],
+      userId: json['userId'],
+      completed: json['completed'],
+    );
+  }
+
+  Map toJson() {
+    return {'id': id, 'title': title, 'completed': completed, 'userId': userId};
+  }
+}
