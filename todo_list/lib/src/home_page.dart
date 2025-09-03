89aed4bba158f8b275029fda8136a3f79fbf5c88
Rafael Filimberti <rfilimberti@gmail.com>
2025-09-03 16:37:32 -0300

    API - ListView.Builder

---
+++ todo_list/lib/src/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('List Todo\'S')),
+
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return ListTile(title: Text('item $index'));
+        },
+      ),
+    );
+  }
+}
