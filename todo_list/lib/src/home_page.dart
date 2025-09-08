ded9c4821c01af9993e434bfcb0bacaf1d238fd0
Rafael Filimberti <rfilimberti@gmail.com>
2025-09-08 15:54:14 -0300

    #52 - [API] Criando os estados da View

--- todo_list/lib/src/home_page.dart
+++ todo_list/lib/src/home_page.dart
@@ -1,17 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/src/controllers/home_controller.dart';
 
 class HomePage extends StatelessWidget {
+  _success() {
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (context, index) {
+        return ListTile(title: Text('item $index'));
+      },
+    );
+  }
+
+  _error() {
+    return Center(
+      child: ElevatedButton(onPressed: () {}, child: Text('tentar novamente')),
+    );
+  }
+
+  _loading() {
+    return Center(child: CircularProgressIndicator());
+  }
+
+  _start() {
+    return Container();
+  }
+
+  stateManagement(HomeState state) {
+    switch (state) {
+      case HomeState.start:
+        return _start();
+
+      case HomeState.loading:
+        return _loading();
+
+      case HomeState.error:
+        return _error();
+
+      case HomeState.success:
+        return _success();
+
+      default:
+        return _start();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('List Todo\'S')),
 
-      body: ListView.builder(
-        itemCount: 20,
-        itemBuilder: (context, index) {
-          return ListTile(title: Text('item $index'));
-        },
-      ),
+      body: stateManagement(HomeState.success),
     );
   }
 }
