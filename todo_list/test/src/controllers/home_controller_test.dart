88bb99123d67a4661081262eb32b3b584e016f9e
Rafael Filimberti <rfilimberti@gmail.com>
2025-09-08 14:27:19 -0300

    #51 - [API] Gerenciamento de Estado

--- todo_list/test/src/controllers/home_controller_test.dart
+++ todo_list/test/src/controllers/home_controller_test.dart
@@ -1,21 +1,40 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:todo_list/src/controllers/home_controller.dart';
 import 'package:todo_list/src/models/todo_model.dart';
 import 'package:todo_list/src/repositories/todo_repository.dart';
 
-class TodoRepositoryMock extends Mock implements TodoRepository {}
+class TodoRepositoryMock extends Mock implements TodoRepository {
+  @override
+  Future<List<TodoModel>> FetchTodos() => super.noSuchMethod(
+    Invocation.method(#FetchTodos, []),
+    returnValue: Future.value(<TodoModel>[]),
+  );
+}
 
 void main() {
   final repository = TodoRepositoryMock();
   final controller = HomeController(repository);
 
-  test('deve preencher variável todos', () async {      
-   
+  test('deve preencher variável todos', () async {
     when(repository.FetchTodos()).thenAnswer((_) async => [TodoModel()]);
 
-    await controller.start();
+    expect(controller.state, HomeState.start);
 
+    await controller.start();
+    expect(controller.state, HomeState.success);
     expect(controller.todos.isNotEmpty, true);
   });
+
+
+  test('deve modificaro estado para error se a requisição falhar', () async {
+    when(repository.FetchTodos()).thenThrow(Exception());
+
+    expect(controller.state, HomeState.start);
+
+    await controller.start();
+    expect(controller.state, HomeState.error);
+  });
+  
 }
